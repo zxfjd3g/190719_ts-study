cd3660f3723347fbea6427338db4ef1aa2df7ef3
zxfjd3g <258147149@qq.com>
2020-02-10 12:44:11 +0800

    函数

--- src/05_函数/test3.ts
+++ src/05_函数/test3.ts
@@ -5,5 +5,20 @@
 需求: 我们有一个add函数，它可以接收2个string类型的参数进行拼接，也可以接收2个number类型的参数进行相加 */
 
 (() => {
-  
+  // 声明2个重载的函数
+  function add (x: string, y: string): string
+  function add (x: number, y: number): number
+
+  // 定义函数实现
+  function add(x: string | number, y: string | number): string | number {
+    // 在实现上我们要注意严格判断两个参数的类型是否相等，而不能简单的写一个 x + y
+    if (typeof x === 'string' && typeof y === 'string') {
+      return x + y
+    } else if (typeof x === 'number' && typeof y === 'number') {
+      return x + y
+    }
+  }
+
+  console.log(add('a', 'b'), add(1, 2))
+  // console.log(add('a', 1)) // error
 })()
